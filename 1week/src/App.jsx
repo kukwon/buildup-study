02bcf3a515b7e4d26b71e2ccb43513ffcbe6c088
kukwon <rnrdnjs415@daum.net>
2025-01-31 18:02:41 +0900

    useReducer 사용

--- 1week/src/App.jsx
+++ 1week/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 import './App.css'
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREASE_TEN':
+      return state + 10
+    case 'DECREASE_TEN':
+      return state - 10
+    default:
+      throw new Error('정의되어있지 않은 action입니다.')
+  }
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, dispatch] = useReducer(reducer, 0)
 
   return (
     <>
       <div>{count}</div>
-      <button onClick={() => setCount((prev) => prev + 10)}>증가</button>
-      <button onClick={() => setCount((prev) => prev - 10)}>감소</button>
+      <button onClick={() => dispatch({ type: 'INCREASE_TEN' })}>증가</button>
+      <button onClick={() => dispatch({ type: 'DECREASE_TEN' })}>감소</button>
     </>
   )
 }
