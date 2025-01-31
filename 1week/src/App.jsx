95f1d96e148a7ea1683fbffc30e3ae816f8bdef6
kukwon <rnrdnjs415@daum.net>
2025-01-31 16:55:03 +0900

    log로 호출순서 파악

--- 1week/src/App.jsx
+++ 1week/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useEffect, useLayoutEffect, useState } from 'react'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [value, setValue] = useState(0)
+
+  //1. useEffect
+  useEffect(() => {
+    if (value === 0) {
+      setValue(10 + Math.random() * 200)
+    }
+    console.log('-- 3. Paint 직후')
+  }, [value])
+
+  //2. useLayoutEffect
+  // useLayoutEffect(() => {
+  //   if (value === 0) {
+  //     setValue(10 + Math.random() * 200)
+  //   }
+  //   console.log('--2. Paint 직전 = React 과정중 (2) Commit 직후')
+  // }, [value])
+
+  console.log('--1. React 과정 중 (1) Render 과정')
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => setValue(0)}>value:{value}</button>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
