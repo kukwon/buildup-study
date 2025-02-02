52ce392a59f6ecf976676ad82adca0f6681ce17b
kukwon <rnrdnjs415@daum.net>
2025-02-02 21:11:07 +0900

    Modal 타입에 따른 컴포넌트 Border-Color 변화

--- 1week/src/App.jsx
+++ 1week/src/App.jsx
@@ -2,9 +2,24 @@ import { createContext, useState, useContext } from 'react'
 import { createPortal } from 'react-dom'
 import './App.css'
 
-function Modal({ title = undefined, content = undefined, onClose }) {
+function Modal({ type = 'info', title = undefined, content = undefined, onClose }) {
+  const color = (type) => {
+    switch (type) {
+      case 'info':
+        return 'blue'
+      case 'success':
+        return 'green'
+      case 'warn':
+        return 'orange'
+      case 'error':
+        return 'red'
+      default:
+        throw new Error('존재하지 않는 모달 타입 입니다.')
+    }
+  }
+
   return (
-    <dialog open>
+    <dialog open style={{ borderColor: color(type) }}>
       <h3>{title}</h3>
       <p>{content}</p>
       <button onClick={(e) => onClose()}>닫기</button>
@@ -18,14 +33,15 @@ const ModalContext = createContext({
 })
 
 function ModalContextProvider({ children }) {
-  const [modal, setModal] = useState({ open: false, title: undefined, content: undefined })
+  const CLOSED = { open: false, type: 'info', title: undefined, content: undefined }
+  const [modal, setModal] = useState(CLOSED)
 
-  function show({ title, content }) {
-    setModal({ open: true, title, content })
+  function show({ type, title, content }) {
+    setModal({ open: true, type, title, content })
   }
 
   function close() {
-    setModal({ open: false, title: undefined, content: undefined })
+    setModal(CLOSED)
   }
 
   return (
@@ -33,7 +49,7 @@ function ModalContextProvider({ children }) {
       {children}
       {modal.open &&
         createPortal(
-          <Modal title={modal.title} content={modal.content} onClose={close} />,
+          <Modal type={modal.type} title={modal.title} content={modal.content} onClose={close} />,
           document.body,
         )}
     </ModalContext.Provider>
@@ -42,22 +58,38 @@ function ModalContextProvider({ children }) {
 
 function InformationModalButton() {
   const { show } = useContext(ModalContext)
-  return <button onClick={(e) => show({ title: '안내', content: '안내 관련 노출' })}>안내</button>
+  return (
+    <button onClick={(e) => show({ title: '안내', content: '안내 관련 노출', type: 'info' })}>
+      안내
+    </button>
+  )
 }
 
 function SuccessModalButton() {
   const { show } = useContext(ModalContext)
-  return <button onClick={(e) => show({ title: '성공', content: '성공 관련 노출' })}>성공</button>
+  return (
+    <button onClick={(e) => show({ title: '성공', content: '성공 관련 노출', type: 'success' })}>
+      성공
+    </button>
+  )
 }
 
 function WarningModalButton() {
   const { show } = useContext(ModalContext)
-  return <button onClick={(e) => show({ title: '경고', content: '경고 관련 노출' })}>경고</button>
+  return (
+    <button onClick={(e) => show({ title: '경고', content: '경고 관련 노출', type: 'warn' })}>
+      경고
+    </button>
+  )
 }
 
 function ErrorModalButton() {
   const { show } = useContext(ModalContext)
-  return <button onClick={(e) => show({ title: '에러', content: '에러 관련 노출' })}>에러</button>
+  return (
+    <button onClick={(e) => show({ title: '에러', content: '에러 관련 노출', type: 'error' })}>
+      에러
+    </button>
+  )
 }
 
 function App() {
