ddcc6377acc8ea62991df1bcfefce9308b17f92b
kukwon <rnrdnjs415@daum.net>
2025-03-04 20:35:22 +0900

    input 요소 register를 통해 useForm에 연결

--- 1week/src/App.jsx
+++ 1week/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import './App.css'
+import { useForm } from 'react-hook-form'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const {
+    handleSubmit,
+    register,
+    watch,
+    formState: { errors },
+  } = useForm()
 
+  function send(data) {
+    console.log('- 검증이 완료된 데이터 : ', data)
+  }
+
+  console.log('- 비제어 컴포넌트 원리 기반의 React Hook Form 은 <input> 입력시 리렌더링 미발생')
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3 style={{ margin: 0 }}>유저 추가하기</h3>
+      <form className='form-wrapper' onSubmit={handleSubmit(send)}>
+        <input className='input' {...register('username')} />
+        <button type='button'>저장안됨</button>
+        <button>저장하기</button>
+        <input type='submit' value='저장하기' />
+      </form>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
